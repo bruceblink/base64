72ab24c2a70bf91c952e300566d0fe5353c94179
likanug <likanug.g@qq.com>
2025-10-03 13:37:52 +0800

    feat: 新增_calc_encode_length()函数，用于计算编码后的字节数

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,10 @@ const std = @import("std");
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
 const lib = @import("base64_lib");
 
-
-pub fn main() !void {
-
-}
-
+pub fn main() !void {}
 
 const Base64 = struct {
+    // Base64 encoding table
     _table: *const [64]u8,
 
     pub fn init() Base64 {
@@ -24,11 +21,20 @@ const Base64 = struct {
         };
     }
 
+    // Get character at index from Base64 table
     pub fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
-};
 
+    // Calculate the length of the encoded output
+    fn _calc_encode_length(input: []const u8) !usize {
+        if (input.len < 3) {
+            return 4;
+        }
+        const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+        return n_groups * 4;
+    }
+};
 
 test "use base64 table" {
     const base64 = Base64.init();
