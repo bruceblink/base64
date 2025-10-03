805f6079a7ad3a60e2203e89eb849ff1369d1a15
likanug <likanug.g@qq.com>
2025-10-03 13:54:52 +0800

    refactor: 调整_calc_encode_length()函数的位置和格式，增加相关注释和单元测试

--- src/main.zig
+++ src/main.zig
@@ -25,18 +25,26 @@ const Base64 = struct {
     pub fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
-
-    // Calculate the length of the encoded output
-    fn _calc_encode_length(input: []const u8) !usize {
-        if (input.len < 3) {
-            return 4;
-        }
-        const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
-        return n_groups * 4;
-    }
 };
+
+// Calculate the length of the encoded output
+fn _calc_encode_length(input: []const u8) !usize {
+    if (input.len < 3) {
+        return 4;
+    }
+    // 除以3向上取整
+    // 每3个字节编码为4个字符
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+    return n_groups * 4;
+}
 
 test "use base64 table" {
     const base64 = Base64.init();
     try std.testing.expectEqual('c', base64._char_at(28));
+}
+
+test "calculate encoded length" {
+    const input = "hello";
+    const encoded_len = try _calc_encode_length(input);
+    try std.testing.expectEqual(8, encoded_len);
 }
